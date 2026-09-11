4ee1b4876e629ed4db090c99f971ca12af5fb18e
paulodirectresponse-jpg <paulo.direct.response@gmail.com>
2026-09-11 02:28:28 -0300

    Add electron/preload.ts

---
+++ electron/preload.ts
@@ -0,0 +1,3 @@
+import { contextBridge, ipcRenderer } from 'electron';
+contextBridge.exposeInMainWorld('forge',{run:(input:string)=>ipcRenderer.invoke('agent:run',input),read:(p:string)=>ipcRenderer.invoke('agent:read',p),checkForUpdates:()=>ipcRenderer.invoke('app:update-check')});
+
