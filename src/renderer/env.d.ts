10c4eb2246913e20283ec4778c416e687644a74f
paulodirectresponse-jpg <paulo.direct.response@gmail.com>
2026-09-11 02:28:55 -0300

    Add src/renderer/env.d.ts

---
+++ src/renderer/env.d.ts
@@ -0,0 +1,2 @@
+export {}; declare global { interface Window { forge:{run:(input:string)=>Promise<any>; read:(p:string)=>Promise<string>; checkForUpdates:()=>Promise<{status:string;message?:string}>}; } }
+
