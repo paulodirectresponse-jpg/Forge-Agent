44ab1ed2f8cce284faee86530d61d88828dbd5a9
paulodirectresponse-jpg <paulo.direct.response@gmail.com>
2026-09-11 02:28:36 -0300

    Add src/core/budget-governor.ts

---
+++ src/core/budget-governor.ts
@@ -0,0 +1,2 @@
+export class BudgetGovernor { constructor(public maxTokens=12000){} canSpend(estimate=0){return estimate<=this.maxTokens;} consume(tokens:number){this.maxTokens=Math.max(0,this.maxTokens-tokens);} }
+
