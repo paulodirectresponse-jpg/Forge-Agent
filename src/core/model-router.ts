fb34d6feb4378f5adebb6180fb0714bddcf4d852
paulodirectresponse-jpg <paulo.direct.response@gmail.com>
2026-09-11 02:28:39 -0300

    Add src/core/model-router.ts

---
+++ src/core/model-router.ts
@@ -0,0 +1,3 @@
+export type Model='useoneai'|'gpt-5.6-luna';
+export class ModelRouter { choose(preferred:Model='useoneai', remainingTokens=10000):Model{return remainingTokens<1000?'gpt-5.6-luna':preferred;} }
+
